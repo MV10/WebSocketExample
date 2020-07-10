7358d414cd77e00a202b4404443a596667b18830
Jon McGuire <mcguirev10@gmail.com>
2020-07-10 16:06:11 -0400

    added Channel-based examples

---
+++ ChannelWSServer/ConnectedClient.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace ChannelWSServer
+{
+    public class ConnectedClient
+    {
+        public ConnectedClient(int socketId, WebSocket socket, TaskCompletionSource<object> taskCompletion)
+        {
+            SocketId = socketId;
+            Socket = socket;
+            TaskCompletion = taskCompletion;
+            BroadcastQueue = Channel.CreateUnbounded<string>(new UnboundedChannelOptions { SingleReader = true, SingleWriter = false });
+        }
+
+        public int SocketId { get; private set; }
+
+        public WebSocket Socket { get; private set; }
+
+        public TaskCompletionSource<object> TaskCompletion { get; private set; }
+
+        public Channel<string> BroadcastQueue { get; private set; }
+
+        public CancellationTokenSource BroadcastLoopTokenSource { get; set; } = new CancellationTokenSource();
+
+        public async Task BroadcastLoopAsync()
+        {
+            var cancellationToken = BroadcastLoopTokenSource.Token;
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                try
+                {
+                    while(await BroadcastQueue.Reader.WaitToReadAsync(cancellationToken))
+                    {
+                        string message = await BroadcastQueue.Reader.ReadAsync();
+                        Console.WriteLine($"Socket {SocketId}: Sending from queue.");
+                        var msgbuf = new ArraySegment<byte>(Encoding.UTF8.GetBytes(message));
+                        await Socket.SendAsync(msgbuf, WebSocketMessageType.Text, endOfMessage: true, CancellationToken.None);
+                    }
+                }
+                catch (OperationCanceledException)
+                {
+                    // normal upon task/token cancellation, disregard
+                }
+                catch (Exception ex)
+                {
+                    Program.ReportException(ex);
+                }
+            }
+        }
+    }
+}
---
+++ ChannelWSServer/Program.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Runtime.CompilerServices;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace ChannelWSServer
+{
+    public class Program
+    {
+        public const int TIMESTAMP_INTERVAL_SEC = 15;
+        public const int BROADCAST_TRANSMIT_INTERVAL_MS = 250;
+        public const int CLOSE_SOCKET_TIMEOUT_MS = 2500;
+
+        public static void Main(string[] args)
+        {
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseUrls(new string[] { @"http://localhost:8080/" });
+                    webBuilder.UseStartup<Startup>();
+                })
+                .Build()
+                .Run();
+        }
+
+        // should use a logger but hey, it's a demo and it's free
+        public static void ReportException(Exception ex, [CallerMemberName]string location = "(Caller name not set)")
+        {
+            Console.WriteLine($"\n{location}:\n  Exception {ex.GetType().Name}: {ex.Message}");
+            if (ex.InnerException != null) Console.WriteLine($"  Inner Exception {ex.InnerException.GetType().Name}: {ex.InnerException.Message}");
+        }
+    }
+}
